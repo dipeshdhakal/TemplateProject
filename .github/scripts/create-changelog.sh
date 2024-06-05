be8e6a67dd9fa22fa4c96beb67fd1f667ba56b91
Dipesh Dhakal <dipesh.dhakal4040@gmail.com>
2024-06-05 13:48:31 +1000

    [NONE-0000] Fix changelog and default patch number (#3)
    Co-authored-by: Dipesh Dhakal <didhakal@expediagroup.com>

--- .github/scripts/create-changelog.sh
+++ .github/scripts/create-changelog.sh
@@ -4,7 +4,7 @@ git fetch --tags origin main
 LAST_TAG="$(git describe --tags --abbrev=0)"
 echo "LAST_TAG: $LAST_TAG"
 
-CHANGE_LOG="$(git log --first-parent --merges --pretty=tformat:"%b (%h)" $LAST_TAG..HEAD | awk '$0="- "$0')"
+CHANGE_LOG="$(git log --first-parent --merges --pretty=tformat:"%b (%h)" $LAST_TAG..HEAD | cat | awk '$0="- "$0')"
 echo "CHANGE_LOG:"
 echo "$CHANGE_LOG"
 
